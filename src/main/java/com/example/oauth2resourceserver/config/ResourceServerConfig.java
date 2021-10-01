f578d123907e1ac4ae337d4450276fb69a9f502d
ChaeRin-Im <ewuuu1112@gmail.com>
2021-10-01 11:23:38 +0900

    JWT 토큰(signKey 방식)

--- src/main/java/com/example/oauth2resourceserver/config/ResourceServerConfig.java
+++ src/main/java/com/example/oauth2resourceserver/config/ResourceServerConfig.java
@@ -1,13 +1,36 @@
 package com.example.oauth2resourceserver.config;
 
+import jdk.nashorn.internal.parser.Token;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+//참고 URL: https://www.skyer9.pe.kr/wordpress/?p=2405
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${security.oauth2.jwt.signkey}")
+    private String signKey;
+
+    @Bean
+    public TokenStore tokenStore(){
+        //JWT 토큰방식(signKey 방식)
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signKey);
+        return converter;
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
